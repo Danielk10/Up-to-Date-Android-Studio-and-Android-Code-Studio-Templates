17bbc70de81525c300fbaccc77405b568fe1b2e4
Danielk10 <danielpdiamon@gmail.com>
2025-11-10 10:06:28 -0400

    Actualización de Plantillas

--- 20.Template Android Studio and android-code-studio libGDX Game Kotlin Gradle 8.14.3 Kotlin DSL AGP 8.9.3/YourProjectName/core/src/main/kotlin/io/github/some_example_name/Main.kt
+++
@@ -1,6 +0,0 @@
-package io.github.some_example_name
-
-import com.badlogic.gdx.ApplicationAdapter
-
-/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
-class Main : ApplicationAdapter()
